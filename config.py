01067a9305de225251a67d89bd586ddc4e1f5f52
charleytrigano <charley@trigano.org>
2026-03-08 15:31:11 +0100

    Add configuration for environment variables

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+BREVO_API_KEY = os.getenv("BREVO_API_KEY")
