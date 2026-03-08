98edf60a2fe4eff0df4398864df34f04015598ba
charleytrigano <charley@trigano.org>
2026-03-08 15:53:54 +0100

    Add Brevo API key and email from environment variables

--- config.py
+++ config.py
@@ -5,4 +5,6 @@ load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 BREVO_API_KEY = os.getenv("BREVO_API_KEY")
+EMAIL_FROM = os.getenv("EMAIL_FROM")
