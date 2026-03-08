b80a2923259a456fb8ec48758123dc3bca6606ee
charleytrigano <charley@trigano.org>
2026-03-08 16:03:37 +0100

    Refactor analytics display and chart creation
    Removed title from analytics display and simplified bar chart creation.

--- pages/analytics.py
+++ pages/analytics.py
@@ -4,19 +4,12 @@ from services.reservation_service import load_reservations
 
 def show():
 
-    st.title("Analyses")
-
     df = load_reservations()
 
     df["mois"] = df["date_arrivee"].dt.month
 
     revenus = df.groupby("mois")["prix_total"].sum().reset_index()
 
-    fig = px.bar(
-        revenus,
-        x="mois",
-        y="prix_total",
-        title="Revenus par mois"
-    )
+    fig = px.bar(revenus, x="mois", y="prix_total")
 
     st.plotly_chart(fig, use_container_width=True)
