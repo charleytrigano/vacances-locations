8a291df06ad3cee27e6ebe51fb433a6e5de0233e
charleytrigano <charley@trigano.org>
2026-03-09 08:06:43 +0100

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,13 +1,21 @@
-page = st.sidebar.radio(
-    "Navigation",
-    [
-        "Dashboard",
-        "Réservations",
-        "Calendrier",
-        "Analyses",
-        "Opportunités",
-        "Ménage",
-        "Messages",
-        "Paramètres"
-    ]
-)
+import streamlit as st
+
+def sidebar():
+
+    st.sidebar.title("🏡 Vacances-Locations")
+
+    page = st.sidebar.radio(
+        "Navigation",
+        [
+            "Dashboard",
+            "Réservations",
+            "Calendrier",
+            "Analyses",
+            "Opportunités",
+            "Ménage",
+            "Messages",
+            "Paramètres"
+        ]
+    )
+
+    return page
